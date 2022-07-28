f6f7810f417d36042e676697572a296731ff8c79
kkm <chachami66@gmail.com>
2022-07-28 11:26:20 +0900

    change config

--- configs/paths_config.py
+++ configs/paths_config.py
@@ -17,12 +17,16 @@ dataset_paths = {
 
 	#  Cats Dataset (In the paper: LSUN Cat)
 	'cats_train': '',
-	'cats_test': ''
+	'cats_test': '',
+
+	'my_train_data': 'added_files/train/',
+	'my_test_data': 'added_files/test/'
 }
 
 model_paths = {
-	'stylegan_ffhq': 'pretrained_models/stylegan2-ffhq-config-f.pt',
+	'stylegan_ffhq': 'added_files/stylegan2-ffhq-config-f.pt',
 	'ir_se50': 'pretrained_models/model_ir_se50.pth',
 	'shape_predictor': 'pretrained_models/shape_predictor_68_face_landmarks.dat',
 	'moco': 'pretrained_models/moco_v2_800ep_pretrain.pth'
 }
+
